9e59dbe1961c44ba440acea4c334c389c6987e09
Lukas Marschall <lmarschall.com@gmail.com>
2020-11-05 12:29:04 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+// const axios = require('axios');
+// const compression = require('compression');
+
+// const api = require('./api');
+
+const port = process.env.PORT || 80;
+
+const history = require('connect-history-api-fallback');
+
+app = express();
+
+app.use(history());
+app.use(express.static(path.join(__dirname, "/app/dist")));
+// app.use('/api', api);
+// app.use(compression()); //COMPRESSION
+
+app.listen(port);
